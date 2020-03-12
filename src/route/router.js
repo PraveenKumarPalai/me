0f5614b7d62a122c2f7b1b433e979219345b6c0e
Praveen Kumar Palai <praveen.palai@outlook.com>
2020-03-12 11:57:55 +0530

    Adde Gh PAges

--- src/route/router.js
+++ src/route/router.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import '../index.css';
 import App from '../App';
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Error from '../pages/error';
 
 const Routes = () => {
     return (
-    <>
-        <Router>
-            <Switch>
-                <Route exact path="/" component={App} />
-                <Route path="https://github.com/PraveenKumarPalai"/>
-                <Route component={Error} />
-            </Switch>
-        </Router>
-    </>
-  )
+        <>
+            <Router>
+                <Switch>
+                    <Route exact path="/me" component={App} />
+                    <Route exact path="/">
+                        <Redirect to="/me" />
+                    </Route>
+                    <Route path="https://github.com/PraveenKumarPalai" />
+                    <Route component={Error} />
+                </Switch>
+            </Router>
+        </>
+    )
 }
 
 export default Routes;
